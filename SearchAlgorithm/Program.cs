e44c53f417249615b9872c2a4dc36625bc27e6fd
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-10-26 16:15:21 +0700

    membuat variabel integer array dan n,i

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        int[] arr = new int[20];
+        int n;
+        int i;
+
+    }
+}
