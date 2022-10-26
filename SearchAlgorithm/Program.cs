abef49a0925ee99f7fadc5791b3ebbb73afa94ba
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-10-26 16:24:18 +0700

    membuat function public void input yang berisi perulangan while dan if

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -12,5 +12,29 @@ namespace SearchAlgorithm
         int n;
         int i;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array:");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element. \n");
+
+                Console.WriteLine("");
+                Console.WriteLine("----------------------");
+                Console.WriteLine(" Enter Array Elements ");
+                Console.WriteLine("----------------------");
+                for (i = 0; i < n; i++)
+                {
+                    Console.Write("<" + (i + 1) + ">");
+                    string s1 = Console.ReadLine();
+                    arr[i] = Int32.Parse(s1);
+                }
+            }
+        }
     }
 }
