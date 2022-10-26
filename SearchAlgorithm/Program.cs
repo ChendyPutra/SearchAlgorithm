9e465c89373a156b3a517deb9061924a6e228d87
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-10-26 17:29:01 +0700

    membuat method static void main untuk memanggil dan menampilkan program yang telah di buat

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -103,7 +103,43 @@ namespace SearchAlgorithm
         }
         static void main(string[] args)
         {
+            Program myList = new Program();
+            int pilihanmenu;
+            char ch;
 
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine("1.Linear Search");
+                    Console.WriteLine("2.Binary Search");
+                    Console.WriteLine("3.Exit");
+                    Console.Write("Enter your choice (1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch(pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine("---------------");
+                            Console.WriteLine(" Linear Search ");
+                            Console.WriteLine("---------------");
+                            myList.input();
+                            myList.LinearSearch();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("---------------");
+                            Console.WriteLine(" Binary Search ");
+                            Console.WriteLine("---------------");
+                            myList.input();
+                            myList.BinarySearch();
+                            break;
+                    }
+
+                }
+            }
         }
     }
 }
